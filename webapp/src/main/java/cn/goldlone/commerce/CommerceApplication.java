c499d4ffefe22c5d20aa07b0ad9828e25ba79b4d
CN <857353825@qq.com>
2018-12-11 23:43:44 +0800

    初始化web模块

---
+++ webapp/src/main/java/cn/goldlone/commerce/CommerceApplication.java
@@ -0,0 +1,12 @@
+package cn.goldlone.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommerceApplication.class, args);
+  }
+}
